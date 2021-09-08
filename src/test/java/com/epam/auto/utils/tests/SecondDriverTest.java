9954456679183fd826330dafe96bd408b17dfc7d
Alexander Kononov1 <alexander_kononov1@epam.com>
2021-09-08 08:20:05 +0300

    xxx

---
+++ src/test/java/com/epam/auto/utils/tests/SecondDriverTest.java
@@ -0,0 +1,33 @@
+package com.epam.auto.utils.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class SecondDriverTest extends BaseTest {
+
+  protected WebDriver secondDriver;
+
+  @BeforeMethod(alwaysRun = true)
+  public void browserSetup() {
+    driver = new ChromeDriver();
+  }
+
+  @BeforeMethod(alwaysRun = true)
+  public void browser2Setup() {
+    secondDriver = new ChromeDriver();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void browserTearDown() {
+    driver.quit();
+    driver = null;
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void browser2TearDown() {
+    secondDriver.quit();
+    secondDriver = null;
+  }
+}
