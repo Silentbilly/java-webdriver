d354127997d3897b3cb29b3b900eaf3e592b0955
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2021-11-10 01:45:53 +0300

    refactoring. deleting unnecessary data provider

--- src/test/java/com/epam/auto/page/BasePage.java
+++ src/test/java/com/epam/auto/page/BasePage.java
@@ -1,25 +1,16 @@
 package com.epam.auto.page;
 
-import com.epam.auto.object.CalculatorData;
-import com.epam.auto.object.CalculatorDataFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.DataProvider;
 
 public abstract class BasePage {
 
-  @DataProvider
-  public static Object[][] pageDataMethod() {
-    CalculatorData defaultCalculatorData = CalculatorDataFactory.createDefaultCalculatorData();
-    return new Object[][]{{defaultCalculatorData.getNumberOfInstances(), defaultCalculatorData.getNumberOfNodes()}};
-  }
-
   protected WebDriver driver;
-
-  protected abstract BasePage openPage();
 
   protected BasePage(WebDriver driver) {
     this.driver = driver;
     PageFactory.initElements(driver, this);
   }
+
+  protected abstract BasePage openPage();
 }
